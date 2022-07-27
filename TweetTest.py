e13afec532cc024738e115609a4de63bc9ff3ec1
praveerOP <praveerdwivedi@gmail.com>
2022-07-27 23:41:40 +0530

    ok

--- TweetTest.py
+++ TweetTest.py
@@ -24,3 +24,30 @@ for tweet in tweets:
     tweets_copy.append(tweet)
     
 print("Total Tweets fetched:", len(tweets_copy))
+
+import pandas as pd
+
+# intialize the dataframe
+tweets_df = pd.DataFrame()
+
+# populate the dataframe
+for tweet in tweets_copy:
+    hashtags = []
+    try:
+        for hashtag in tweet.entities["hashtags"]:
+            hashtags.append(hashtag["text"])
+        text = api.get_status(id=tweet.id, tweet_mode='extended').full_text
+    except:
+        pass
+    tweets_df = tweets_df.append(pd.DataFrame({'user_name': tweet.user.name, 
+                                               'user_location': tweet.user.location,\
+                                               'user_description': tweet.user.description,
+                                               'user_verified': tweet.user.verified,
+                                               'date': tweet.created_at,
+                                               'text': text, 
+                                               'hashtags': [hashtags if hashtags else None],
+                                               'source': tweet.source}))
+    tweets_df = tweets_df.reset_index(drop=True)
+
+# show the dataframe
+tweets_df.head()

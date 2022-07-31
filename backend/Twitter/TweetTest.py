bb8a5c2103c4f444eb858f26b4d9385648d658f7
Sourabh <sourabhjamdar1@gmail.com>
2022-07-31 22:11:10 +0530

    Last Submission

--- backend/Twitter/TweetTest.py
+++ backend/Twitter/TweetTest.py
@@ -3,7 +3,7 @@ import tweepy as tw
 import pandas as pd
 
 def twitter():
-    # your Twitter API key and API secret
+    #Twitter API key and API secret
     access_token = "1551145058258153473-utDfnXHO0FX3N1RYKP57AeldcDXZSm"
     access_token_secret = "bOqz0kjpCnhtiRkpYPzVVGwiiZJnQfYMu8z2rbqecPPYh"
 
@@ -16,24 +16,20 @@ def twitter():
     tweets_copy = []
     tweets_df = pd.DataFrame()
     phone_list=["#samsung","#xiaomi","#moto","#vivo","#oppo","#iPhone","#redmi","#apple",
-    "#asus","#lenovo","#hp","#"]
+    "#asus","#lenovo"]
     for i in phone_list:
         search_query = i
     # get tweets from the API
         tweets = tw.Cursor(api.search_tweets,
                     q=search_query,
                     lang="en",
-                    since="2020-09-16").items(10)
+                    since="2021-07-01").items(50)
 
     # store the API responses in a list
-
-        
         for tweet in tweets:
             tweets_copy.append(tweet)
         
     print("Total Tweets fetched:", len(tweets_copy))
-    # intialize the dataframe
-    # populate the dataframe
     for tweet in tweets_copy:
         hashtags = []
         try:
@@ -46,9 +42,6 @@ def twitter():
                                                 'user_description': tweet.user.description,
                                                 'hashtags': [hashtags if hashtags else None]
                                                 }))
-        # tweets_df = tweets_df.reset_index(drop=True)
-    # show the dataframe
-    # print(tweets_df)
     print(tweets_df.head())
     tweets_list = tweets_df['hashtags'].tolist()
     tweets_desc = tweets_df['user_description'].tolist()
@@ -56,41 +49,17 @@ def twitter():
     for k in tweets_list:
         if k != None:
             tweets_newlist.extend(k)
-
-    # for i in tweets_df['hashtags']:
-    #     if i[0]!=None:
-    #         tweets_list.extend(i[0])
     frequency={}
     for item in tweets_newlist:
-        # checking the element in dictionary
         if item in frequency:
-        # incrementing the counr
+
             frequency[item] += 1
         else:
-        # initializing the count
             frequency[item] = 1
-    # printing the frequency
-    #print (tweets_list)
     frequency=dict(sorted(frequency.items(), key=lambda item: item[1],reverse=True))
     print(frequency)
-    # return frequency
-    #possible extractable feature 
-    # fet=[]
-    # possible_list=["Samsung Galaxy","Oppo"]
-    # tweet_list=[]
     print(tweets_desc)
-    # print("-----------------------------------------")
-    # print("-----------------------------------------")
-    print(tweets_newlist)
-    given_list=['realme C11 2021 ', 'realme C11 2021 ', 
-    'SAMSUNG Galaxy F22 ', 'SAMSUNG Galaxy F22 ', 'realme C31 ', 'realme C31 ', 'POCO C31 ', 'realme C31 ', 
-    'Infinix HOT 12 Play ', 'Infinix HOT 12 Play ', 'Infinix HOT 12 Play ', 'realme C31 ', 'POCO C31 ', 'realme C11 2021 ', 
-    'POCO C31 ', 'realme 9', 'Infinix Note 12 ', 'Infinix Hot 11 2022 ', 'SAMSUNG Galaxy F13 ', 
-    'SAMSUNG Galaxy F13 ', 'realme C20 ', 'Infinix Note 12 ', 'POCO M4 Pro ', 'realme 9 ', 'OPPO K10 ', 
-    'REDMI 9i Sport ', 'realme C20', 'SAMSUNG Galaxy F13 ', 'vivo T1X ', 'vivo T1X ', 
-    'vivo T1X ', 'vivo T1X ', 'REDMI 10 ','REDMI 10 ', 'MOTOROLA g31 ', 'REDMI 9i Sport ', 'POCO M4 Pro ',
-    'REDMI Note 10 Pro ', 'vivo T1X ', 'realme C21Y ', 'REDMI Note 10T 5G ',
-    'Infinix Hot 11 2022 ', 'OPPO Reno8 5G ', 'OPPO Reno8 5G ', 'realme 9 ', 'REDMI Note 10 Pro ', 'POCO M4 5G ']    
-
-twitter();
+    print(tweets_newlist)  
+    return frequency, tweets_desc
+# twitter();
 
--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,46 @@
 from typing import Union
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from fuzzywuzzy import process
+import re
+from Flipkart.flipkartdata import flipkart
 from Twitter.TweetTest import twitter
+
 tweets = []
-tweets_dict = twitter()
-# for i in tweets_df:
-#     print(i)
-# print(tweets_df.head())
-# print(tweets_df)
+tweets_dict, tweets_desc = twitter()#get twitter data
+data = flipkart()#get flipkart data
+
+names_match = []
+for i in data["Product_name"]:
+  names_match.append(re.sub("[\(\[].*?[\)\]]", "", i))
+print(names_match, len(names_match))
+print("Tweets_desc: ", tweets_desc)
+str2Match = "|".join(tweets_desc)
+print("Str2match",str2Match)
+strOptions = names_match
+Ratios = process.extract(str2Match,strOptions)
+print("Ratios : ",Ratios)
+
+#Matching twitter trends with flipkart data
+strOptions = names_match
+matched_entries = []
+for name in str2Match:
+   Ratios = process.extract(name,strOptions, limit = 1)
+   matched_entries.append(Ratios)
+# print(matched_entries)
+
+#Calculating most frequent product trends and storing sorted results in non ascending order
+matched_dict = {}
+for i in matched_entries:
+  if(i[0][0]in matched_dict):
+    matched_dict[i[0][0]] += i[0][1] 
+  else:
+    matched_dict[i[0][0]] = i[0][1] 
+item_dict = dict(sorted(matched_dict.items(), key=lambda item: item[1], reverse = True))
+# print(item_dict)
+
+#Creating endpoints to access different product categories 
 app = FastAPI()
-@app.get("/")
+@app.get("/smartphones")
 def read_root():
-    return {"Smartphones":tweets_dict }
+    return {"Smartphones":item_dict }
 
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
